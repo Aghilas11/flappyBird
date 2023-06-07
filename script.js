7c1ddd586fe6842bc9aa9cd939c0e7beb5fa9a44
AGHILAS <halouane.aghilas@yahoo.com>
2023-06-07 17:54:05 +0200

    floppy bird

--- script.js
+++ script.js
@@ -5,18 +5,33 @@ img.src = "./media/flappy-bird-set.png";
 
 // General Settings
 let gamePlaying = false;
-const gravity = 5;
+const gravity = 0.5;
 const speed = 6.2;
 const size = [51, 36];
 const jump = -11.5;
 const cTenth = canvas.width / 10;
 
+// Pipe settings
+const pipeWidth = 78;
+const pipeGap = 270;
+const pipeLoc = () =>
+  Math.random() * (canvas.height - (pipeGap + pipeWidth) - pipeWidth) +
+  pipeWidth;
 let index = 0,
   bestScore = 0,
   currentScore = 0,
   pipes = [],
   flight,
   flyHeight;
+
+const setup = () => {
+  currentScore = 0;
+  flight = jump;
+  flyHeight = canvas.height / 2 - size[1] / 2;
+  pipes = Array(3)
+    .fill()
+    .map((a, i) => [canvas.width + i * (pipeGap + pipeWidth), pipeLoc()]);
+};
 
 const render = () => {
   index++;
@@ -46,20 +61,99 @@ const render = () => {
     canvas.height
   );
 
-  ctx.drawImage(
-    img,
-    432,
-    Math.floor((index % 9) / 3) * size[1],
-    ...size,
-    canvas.width / 2 - size[0] / 2,
-    flyHeight,
-    ...size
-  );
-  flyHeight = canvas.height / 2 - size[1] / 2;
+  if (gamePlaying) {
+    ctx.drawImage(
+      img,
+      432,
+      Math.floor((index % 9) / 3) * size[1],
+      ...size,
+      cTenth,
+      flyHeight,
+      ...size
+    );
+    flight += gravity;
+    flyHeight = Math.min(flyHeight + flight, canvas.height - size[1] / 2);
+  } else {
+    ctx.drawImage(
+      img,
+      432,
+      Math.floor((index % 9) / 3) * size[1],
+      ...size,
+      canvas.width / 2 - size[0] / 2,
+      flyHeight,
+      ...size
+    );
+    flyHeight = canvas.height / 2 - size[1] / 2;
+
+    ctx.fillText(`Meilleur score :  ${bestScore}`, 50, 245);
+    ctx.fillText("Cliquez pour jouer", 48, 535);
+    ctx.font = "bold 30px courier ";
+  }
+
+  // Pipe display (Affichage de pauteaux)
+  if (gamePlaying) {
+    pipes.map((pipe) => {
+      pipe[0] -= speed;
+      // Poteau du haut
+      ctx.drawImage(
+        img,
+        432,
+        588 - pipe[1],
+        pipeWidth,
+        pipe[1],
+        pipe[0],
+        0,
+        pipeWidth,
+        pipe[1]
+      );
+
+      // Poteau du bas
+      ctx.drawImage(
+        img,
+        432 + pipeWidth,
+        108,
+        pipeWidth,
+        canvas.height - pipe[1] + pipeGap,
+        pipe[0],
+        pipe[1] + pipeGap,
+        pipeWidth,
+        canvas.height - pipe[1] + pipeGap
+      );
+
+      if (pipe[0] <= -pipeWidth) {
+        currentScore++;
+        bestScore = Math.max(bestScore, currentScore);
+
+        // renouveler les poteaux
+        pipes = [
+          ...pipes.slice(1),
+          [pipes[pipes.length - 1][0] + pipeGap + pipeWidth, pipeLoc()],
+        ];
+      }
+
+      // Perde si on touche le poteau
+      if (
+        [
+          pipe[0] < cTenth + size[0],
+          pipe[0] + pipeWidth >= cTenth,
+          pipe[1] > flyHeight || pipe[1] + pipeGap < flyHeight + size[1],
+        ].every((elem) => elem)
+      ) {
+        gamePlaying = false;
+        setup();
+      }
+    });
+  }
+
+  document.getElementById("bestScore").innerHTML = ` Meilleur : ${bestScore} `;
+  document.getElementById(
+    "currentScore"
+  ).innerHTML = ` Actuel : ${currentScore} `;
 
   window.requestAnimationFrame(render);
 };
 
+setup();
 img.onload = render;
-
-//24:20
+document.addEventListener("click", () => (gamePlaying = true));
+window.onclick = () => (flight = jump);
